e572517d72223b52dc53cd76a83e17973320f647
utkarsh raj <utkarshbrb6@gmail.com>
2025-07-20 02:07:52 +0530

    Adding User Model and Video Model; using JWT and bcrypt

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+
+app.use(express.json({limit:"16kb"})) // for take json data or converting into json file
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //for taking data object inside the object
+app.use(express.static("public")) //for store the data like image into the public folder
+app.use(cookieParser()) // cookie jo store hota hai use is trh se convert kr deta hai ki use aur koi pdh nhi sakta hai sivay server ke
+
 
 
 export {app}
